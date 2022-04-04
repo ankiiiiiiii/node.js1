54ba51a46e602481c6716a9cb531d092f6591c1c
ankiiiiiiii <67005296+ankiiiiiiii@users.noreply.github.com>
2022-04-04 16:45:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express()
 const port = 3100;
 
-app.get('/', (req, res) => res.send('Welcome!'));
+app.get('/', (req, res) => res.send('Welcome! this is nodejs simple website running'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
